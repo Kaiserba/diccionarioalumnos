0af0e0d4d78cab9c3c46563462e3e75892bfd1a6
Kaiserba <jgabrielcortezz@hotmail.com>
2020-12-30 12:22:50 -0300

    nuevo

---
+++ prueba.py
@@ -0,0 +1,22 @@
+
+cursos ={
+   "cursoa" :{
+        "Alumnos" :{
+            "Gabiel" : {"Edad" : "26"},
+            "Jorge" : {"Edad" : "36"},
+   }
+  },
+   "cursob" :{
+        "Alumnos" :{
+            "jose" : {"Edad" : "26"},
+            "ricardo" : {"Edad" : "36"},
+   }
+  }
+
+
+}
+
+
+
+
+print(cursos["cursoa"]["Alumnos"]["Gabiel"]["Edad"])
